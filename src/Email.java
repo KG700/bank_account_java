112574f8e5a51ff0ed309c03dc08f82ec2618772
Karla Gardiner <karla.gardiner@gmail.com>
2020-09-28 13:39:39 +0100

    Set up changePassword and setMailCapacity methods

--- src/Email.java
+++ src/Email.java
@@ -2,14 +2,31 @@ public class Email {
 
     String firstName;
     String lastName;
+    String department;
+    String emailAddress;
+    String password;
+    int mailCapacity;
 
-    Email(String firstName, String lastName) {
+    Email(String firstName, String lastName, String department) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
 
-    String generate() {
+    void generateEmail() {
         return firstName + "." + lastName + "@department.company.com";
     }
+
+    void generatePassword() {
+        return null;
+    }
+
+    void changePassword() {
+
+    }
+
+    void setMailCapacity(int capacity) {
+        mailCapacity = capacity;
+    }
+
 
 }
