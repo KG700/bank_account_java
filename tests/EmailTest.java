ad169c93aa977a353063c7c9cda158aadc2227e9
Karla Gardiner <karla.gardiner@gmail.com>
2020-09-28 18:42:09 +0100

    Refactored tests to initiate email in beforeeach

--- tests/EmailTest.java
+++ tests/EmailTest.java
@@ -1,55 +1,58 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-import java.math.BigInteger;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmailTest {
+
+    private Email amyEmail;
+
+    @BeforeEach
+    void myTestSetUp() {
+        amyEmail = new Email("Amy", "Johnson");
+    }
+
     @Test
     public void getFirstName() throws Exception {
-        Email em1 = new Email("Amy", "Johnson");
-        assertEquals("Amy", em1.getFirstName());
+        assertEquals("Amy", amyEmail.getFirstName());
     }
 
     @Test
     public void getSalesDepartmentFromUser() {
-        Email em1 = new Email("Amy", "Johnson");
         String userInput = "1";
         InputStream in = new ByteArrayInputStream(userInput.getBytes());
         System.setIn(in);
 
-        assertEquals("sales", em1.setDepartment());
+        assertEquals("sales", amyEmail.setDepartment());
     }
 
     @Test
     public void getDevelopmentDepartmentFromUser() {
-        Email em1 = new Email("Amy", "Johnson");
         String userInput = "2";
         InputStream in = new ByteArrayInputStream(userInput.getBytes());
         System.setIn(in);
 
-        assertEquals("development", em1.setDepartment());
+        assertEquals("development", amyEmail.setDepartment());
     }
 
     @Test
     public void getAccountingDepartmentFromUser() {
-        Email em1 = new Email("Amy", "Johnson");
         String userInput = "3";
         InputStream in = new ByteArrayInputStream(userInput.getBytes());
         System.setIn(in);
 
-        assertEquals("accounting", em1.setDepartment());
+        assertEquals("accounting", amyEmail.setDepartment());
     }
 
     @Test
     public void getNoDepartmentFromUser() {
-        Email em1 = new Email("Amy", "Johnson");
         String userInput = "0";
         InputStream in = new ByteArrayInputStream(userInput.getBytes());
         System.setIn(in);
 
-        assertEquals("", em1.setDepartment());
+        assertEquals("", amyEmail.setDepartment());
     }
 }
