36da7815db4869ea46d1e1c634bb81a6061924d9
Karla Gardiner <karla.gardiner@gmail.com>
2020-09-28 13:58:46 +0100

    Created Email object in main to check it is correctly set up

--- src/Email.java
+++ src/Email.java
@@ -8,43 +8,44 @@ public class Email {
     private String alternativeEmail;
     private int mailCapacity;
 
-    Email(String firstName, String lastName, String department) {
+    public Email(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.department = department;
+
+        System.out.println("Email created: " + this.firstName + " " + this.lastName);
     }
 
-    void generateEmail() {
-        emailAddress = firstName + "." + lastName + "@" + department + ".company.com";
-    }
-
-    void generatePassword() {
-        password = "ABC";
-    }
-
-    void changePassword(String newPassword) {
-        password = newPassword;
-    }
-
-    void setMailCapacity(int capacity) {
-        mailCapacity = capacity;
-    }
-
-    void setAlternativeEmail(String altEmail) {
-        alternativeEmail = altEmail;
-    }
-
-    String getName() {
-        return firstName + " " + lastName;
-    }
-
-    String getEmail() {
-        return emailAddress;
-    }
-
-    int getMailCapacity() {
-        return mailCapacity;
-    }
+//    void generateEmail() {
+//        emailAddress = firstName + "." + lastName + "@" + department + ".company.com";
+//    }
+//
+//    void generatePassword() {
+//        password = "ABC";
+//    }
+//
+//    void changePassword(String newPassword) {
+//        password = newPassword;
+//    }
+//
+//    void setMailCapacity(int capacity) {
+//        mailCapacity = capacity;
+//    }
+//
+//    void setAlternativeEmail(String altEmail) {
+//        alternativeEmail = altEmail;
+//    }
+//
+//    String getName() {
+//        return firstName + " " + lastName;
+//    }
+//
+//    String getEmail() {
+//        return emailAddress;
+//    }
+//
+//    int getMailCapacity() {
+//        return mailCapacity;
+//    }
 
 
 }
--- src/EmailApplication.java
+++ src/EmailApplication.java
@@ -1,5 +1,5 @@
 public class EmailApplication {
     public static void main(String[] args) {
-
+        Email em1 = new Email("Karla", "Gardiner");
     }
 }
